29c6f4634c31a28a8e680096ad87ec357e312293
arakakitatsuki <20yuteo@gmail.com>
2022-01-03 23:17:55 +0900

    todo form componentの追加

---
+++ src /src/components/TodoForm.vue
@@ -0,0 +1,30 @@
+<template>
+    <form action="" @submit.prevent="handleSubmit">
+        <label for="title">
+            Title
+        </label>
+        <input type="text" v-model="title" />
+        <input type="submit">
+    </form>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  emits: ['setTodo'],
+  setup (props, { emit }) {
+    const title = ref('')
+
+    const handleSubmit = () => {
+      emit('setTodo', title.value)
+      title.value = ''
+    }
+
+    return {
+      title,
+      handleSubmit
+    }
+  }
+})
+</script>
