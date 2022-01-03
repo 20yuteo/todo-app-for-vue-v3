b6abb92d26e12245503efa7307087661848ae3f6
arakakitatsuki <20yuteo@gmail.com>
2022-01-03 23:17:38 +0900

    todo componentの追加

---
+++ src /src/components/Todo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <span>
+          id: {{ id }}
+        </span>
+        <span>
+          title: {{ title }}
+        </span>
+        <span style="color: red">
+          {{ isDone ? 'done' : null }}
+        </span>
+        <input v-if="!isDone" type="checkbox" @change="handleChange" />
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    id: Number,
+    title: String,
+    isDone: Boolean
+  },
+  emits: ['doneTodo'],
+
+  setup (props, { emit }) {
+    const checkedTodo = []
+
+    const handleChange = () => {
+      emit('doneTodo', props.id)
+    }
+
+    return {
+      checkedTodo,
+      handleChange
+    }
+  }
+})
+</script>
